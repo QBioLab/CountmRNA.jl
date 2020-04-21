ecdc105e7db83568df609515d7ceb67f92985dcf
HF <moyuejian@outlook.com>
2020-04-21 15:15:06 +0800

    update tiffxml.jl using exiftool

--- tiffxml.jl
+++ tiffxml.jl
@@ -1,6 +1,12 @@
 using LightXML
 """
-20200420 hf first version
+Verision Comment
+v0.1     hf, firstversion
+v0.2     hf, use exiftool instead of tiffcommit
+
+ref: http://web.mit.edu/jhawk/mnt/cgs/Image-ExifTool-6.99/html/exiftool_pod.html
+    https://exiftool.org/TagNames/EXIF.html
+    https://docs.openmicroscopy.org/ome-model/5.6.3/ome-tiff/specification.html
 """
 
 """
@@ -22,11 +28,12 @@ function generatexml(SizeX::Integer, SizeY::Integer, SizeZ::Integer, SizeT::Inte
     set_attributes(Image, Dict("ID"=>"Image:0", "Name"=>name))
     
     Pixels = new_child(Image, "Pixels");
-    #SizeC=1; SizeT=10;  SizeX=1300; SizeY=1900; SizeZ=2;
+    SizeC=1; 
+    #SizeT=10;  #SizeX=1300; #SizeY=1900; #SizeZ=2;
     PhysicalSizeX=PhysicalSizeY=0.108; PhysicalSizeZ=0.5;
     TimeIncrement=600;
     Pixels_attributes = Dict("ID"=>"Pixels:0","Type"=>"uint16", "BigEndian"=>"false",
-    "DimensionOrder"=>"XYZCT", "SizeC"=>SizeC,"SizeX"=>SizeX, "SizeY"=>SizeY, "SizeZ"=>SizeZ, "SizeT"=>SizeT,
+    "DimensionOrder"=>"XYZCT", "SizeC"=>1,"SizeX"=>SizeX, "SizeY"=>SizeY, "SizeZ"=>SizeZ, "SizeT"=>SizeT,
     "PhysicalSizeX"=>PhysicalSizeX, "PhysicalSizeY"=>PhysicalSizeY, "PhysicalSizeZ"=>PhysicalSizeZ,
     "PhysicalSizeXUnit"=>"µm", "PhysicalSizeYUnit"=>"µm", "PhysicalSizeZUnit"=>"µm",
     "TimeIncrement"=>TimeIncrement, "TimeIncrementUnit"=>"s");
@@ -49,6 +56,12 @@ Embed OME-XML to tiff with XYZT informance
 function embedxml(SizeX::Integer, SizeY::Integer, SizeZ::Integer, SizeT::Integer, img_name::String)
     tiffxml = generatexml(SizeX, SizeY, SizeZ, SizeT);
     print("Embedding OME-XML")
-    run(`/home/hf/Bin/bftools/tiffcomment -set $tiffxml $img_name`);
+    #run(`/home/hf/Bin/bftools/tiffcomment -set $tiffxml $img_name`);
+    if Sys.which("exiftool") == Nothing
+        print("Please install exiftool")
+    else
+        run(`exiftool -ImageDescription=$tiffxml $img_name -overwrite_original`)
+        #run(`rm $img_name"_original"`)
+    end
     nothing;
 end
