e06b0037b2cb58afeb5d58298fc318ff7e7d8d1e
H.F <moyuejian@outlook.com>
2021-03-03 16:39:25 +0800

    remove Scene and Scene8 if they exist

--- ims_cp.jl
+++ ims_cp.jl
@@ -1,8 +1,10 @@
 using LightXML
 using JLD
- 
+using HDF5 
 """
-Usage: julia ims_cp.jl imaris_root_dir exp_folder_name imaris_creation_name destination
+Usage:
+    julia ims_cp.jl imaris_root_dir exp_folder_name imaris_creation_name destination
+
 Version  Commit
 0.1      first version
 TODO: combine scene and image file
@@ -11,6 +13,7 @@ TODO: combine scene and image file
 "copy and rename imaris batch result from imaris database with Folder and Creation name"
 function ims_cp(ims_root::String, ims_array::String, ims_creation::String, output::String)
     id2name = Dict()
+    id2path= Dict()
     # load ims TreeData.xml
     TREEDATE = parse_file("$ims_root/TreeData.xml")
     # enter root node
@@ -31,7 +34,10 @@ function ims_cp(ims_root::String, ims_array::String, ims_creation::String, outpu
                                 if name(e) == "Image"
                                     image_name = attribute(e, "name")
                                     image_id = attribute(e, "id")
+                                    image_path = replace( attribute(e, "path"), 
+                                                         "file:///C%3A/Imaris_Data_qblab/"=>"" )
                                     push!(id2name, image_id=>image_name)
+                                    push!(id2path, image_id=>image_path)
                                 elseif name(e) == "CreationParameter"
                                     # get CreationParameter id
                                     creation_parameters = attribute(e, "CreationParameters")
@@ -49,20 +55,33 @@ function ims_cp(ims_root::String, ims_array::String, ims_creation::String, outpu
                                     batchrunresults = e["batchrunresults"][1]["result"]
                                     for result in batchrunresults
                                         if attribute(result, "status") == "FINISHED"
-                                            input = attribute(result, "input")
+                                            id = attribute(result, "input")
                                             outputpath = attribute(result, "outputpath")
-                                            src = replace(outputpath, "file:/"=>"$ims_root")
-                                            out = "$output/$(id2name[input]).ims";
-                                            print(id2name[input], " ")
-                                            try
-                                                cp(src, out)
-                                            catch
-                                                
-                                            end 
+                                            src_scene = replace(outputpath, "file:/"=>"$ims_root")
+                                            src_ims = "$ims_root/$(id2path[id])"
+                                            out_ims = "$output/$(id2name[id]).ims";
+                                            print(id2name[id], " ")
+                                            cp(src_ims, out_ims, force=true)
+                                            ims_f = h5open(out_ims, "r+")
+                                            scene_f = h5open(src_scene, "r")
+                                            if haskey(ims_f, "Scene")
+                                                HDF5.delete_object(ims_f, "Scene")
+                                            end
+                                            if haskey(ims_f, "Scene8")
+                                                HDF5.delete_object(ims_f, "Scene8")
+                                            end
+                                            HDF5.copy_object(scene_f, "Scene", ims_f, "Scene")
+                                            HDF5.copy_object(scene_f, "Scene8", ims_f, "Scene8")
+                                            close(ims_f)
+                                            close(scene_f)
                                         end
                                     end
+                                else
+                                    #println("$ims_creation no found!")
                                 end
                             end
+                        else
+                            #println("$ims_array no found!")
                         end
                     end
                 end
